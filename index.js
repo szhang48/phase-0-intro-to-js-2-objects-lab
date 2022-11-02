baf1654b26d6e44ef4b0a94c80eea7557d6ad0b6
Sandy Zhang <sandyzhang142@gmail.com>
2022-11-01 20:00:51 -0400

    commit tree 5c335ce5288486f02a9a598046037bbdaceb4fca

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+const employee = {
+    name: "Sandy",
+    streetAddress: "11 Broadway",
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    const newemployee = {...employee };
+    newemployee[key] = value;
+    return newemployee;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+   const newemployee = {...employee};
+   delete newemployee[key];
+   return newemployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employees, key) { 
+    delete employees[key];
+    return employees;
+
+}
